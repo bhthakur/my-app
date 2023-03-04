d771b88b7d57606c23792f6eb2ccca2e4a1932a2
thakurbhoop <bhoopthakur@hotmail.com>
2023-03-05 10:19:51 +1100

    takeout unwanted code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+
   return (
     <div className="App">
-     
+      'Create form'
     </div>
   );
 }

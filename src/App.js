f529bc0504142781bc37200aef6a87ab505acec5
thakurbhoop <bhoopthakur@hotmail.com>
2023-03-05 10:15:57 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import "./App.css";
 
-import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: "todo1",
+      createdAt: "18:00",
+    },
+    {
+      id: "todo2",
+      createdAt: "16:00",
+    },
+  ]);
+  const reverse= () => {
+    setTodos([...todos].reverse());
+  }
   return (
     <div className="App">
-     
+      'Create form'
     </div>
   );
 }
